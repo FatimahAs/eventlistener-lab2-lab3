316ef5e5d27f3b1a05703ccb4bf2210bdb42a678
FATIMAH ASIRI <fatimahasiri@FATIMAHs-MacBook-Pro-2.local>
2025-05-08 11:21:19 +0300

    add lab2

---
+++ main.js
@@ -0,0 +1,69 @@
+let div = document.getElementById("screen")
+let one = document.getElementById("one")
+let two = document.getElementById("two")
+let three = document.getElementById("three")
+let four = document.getElementById("four")
+let five = document.getElementById("five")
+let six = document.getElementById("six")
+let seven = document.getElementById("seven")
+let eight = document.getElementById("eight")
+let nine = document.getElementById("nine")
+
+one.addEventListener("click", () => {
+    let img = document.getElementById("show")
+    img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg"
+
+})
+
+
+
+two.addEventListener("click", () => {
+    let img = document.getElementById("show")
+    img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg"
+
+})
+
+three.addEventListener("click", () => {
+    let img = document.getElementById("show")
+    img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg"
+
+})
+
+four.addEventListener("click", () => {
+    let img = document.getElementById("show")
+    img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg"
+
+})
+
+
+five.addEventListener("click", () => {
+    let img = document.getElementById("show")
+    img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg"
+
+})
+
+
+six.addEventListener("click", () => {
+    let img = document.getElementById("show")
+    img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg"
+
+})
+
+
+seven.addEventListener("click", () => {
+    let img = document.getElementById("show")
+    img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg"
+
+})
+
+eight.addEventListener("click", () => {
+    let img = document.getElementById("show")
+    img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg"
+
+})
+
+nine.addEventListener("click", () => {
+    let img = document.getElementById("show")
+    img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg"
+
+})
